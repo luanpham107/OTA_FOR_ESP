c1e93fa8f0d3aa084e4b5b13a25bde0092a9af19
Liam <liam@enclave.vn>
2018-04-19 16:09:12 +0700

    Add user class, mysql connector

---
+++ README.md
@@ -0,0 +1,4 @@
+## Firmware information 
+Firmware management for ESP8266 and ESP32 <br/>
+Spring version view in pom.xml <br/>
+Enter path localhost:8080/firmwareinfo
---
+++ src/main/java/ota/User.java
@@ -0,0 +1,48 @@
+package ota;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String password;
+    private boolean enable;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnable() {
+        return enable;
+    }
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+}
